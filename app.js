47701b61ce134a09b35a69423199489e9a3758d6
nocTKpunTyM <noctkpuntym@yandex.ru>
2023-08-18 16:10:29 +0300

    Ссылки на проект, убрал роуты из апп, лимитер отдельно

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const helmet = require('helmet');
-const rateLimit = require('express-rate-limit');
 const { errors } = require('celebrate');
-const NotFoundError = require('./errors/not-found-err');
-const { signinValidation, signupValidation } = require('./middlewares/celebrate');
+const limiter = require('./utils/limiter');
+const routers = require('./routes');
 const cors = require('./middlewares/cors');
 
 const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
@@ -14,12 +13,7 @@ const app = express();
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 app.use(helmet());
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
-  max: 100,
-  standardHeaders: true,
-  legacyHeaders: false,
-});
+
 app.use(limiter);
 
 app.use(cors);
@@ -30,23 +24,12 @@ app.get('/crash-test', () => {
   }, 0);
 });
 
-const usersRouter = require('./routes/users');
-const moviesRouter = require('./routes/movies');
-
-const { createUser, login } = require('./controllers/users');
-const auth = require('./middlewares/auth');
 const errorsMid = require('./middlewares/errors');
 
 app.use(express.json());
-app.use(requestLogger); // подключаем логгер запросов
+app.use(requestLogger);
 
-app.post('/signin', signinValidation, login);
-app.post('/signup', signupValidation, createUser);
-app.use('/users', auth, usersRouter);
-app.use('/movies', auth, moviesRouter);
-app.use((req, res, next) => {
-  next(new NotFoundError({ message: 'Страница не найдена' }));
-});
+app.use(routers);
 
 app.use(errorLogger);
 app.use(errors());
---
+++ routes/index.js
@@ -0,0 +1,19 @@
+const router = require('express').Router();
+const NotFoundError = require('../errors/not-found-err');
+const { signinValidation, signupValidation } = require('../middlewares/celebrate');
+
+const usersRouter = require('./users');
+const moviesRouter = require('./movies');
+
+const { createUser, login } = require('../controllers/users');
+const auth = require('../middlewares/auth');
+
+router.post('/signin', signinValidation, login);
+router.post('/signup', signupValidation, createUser);
+router.use('/users', auth, usersRouter);
+router.use('/movies', auth, moviesRouter);
+router.use((req, res, next) => {
+  next(new NotFoundError({ message: 'Страница не найдена' }));
+});
+
+module.exports = router;
